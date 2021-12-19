b111c2286ddb29d4f22782db65cea6cc559754e8
cachama17 <nicojaimes1903@gmail.com>
2021-12-19 10:17:18 -0500

    MercadoLiebre Deploy

--- app.js
+++ app.js
@@ -11,6 +11,5 @@ app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"./views/home.html"))
 app.get("/registro",(req,res)=>res.sendFile(path.resolve(__dirname,"./views/registro.html")))
 app.get("/ingreso",(req,res)=>res.sendFile(path.resolve(__dirname,"./views/ingreso.html")))
 
-app.listen(3000, () => {console.log("servidor corriendo en el puerto 3000");
-});
+app.listen( process.env.PORT || 3000, () => console.log("Servidor corriendo en Puerto: 3000"));
 
